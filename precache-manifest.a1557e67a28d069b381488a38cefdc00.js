f7789dea7cec734065511311923d740a3eb7498d
Maksim Shakhlin <maksim-shakhlin@yandex.ru>
2020-08-24 01:01:49 +0500

    Updates

---
+++ precache-manifest.a1557e67a28d069b381488a38cefdc00.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5d400762e8217d5ab73b231074b0f5c",
+    "url": "/estate-test/index.html"
+  },
+  {
+    "revision": "fdf859f93ebe486546c0",
+    "url": "/estate-test/static/css/main.9487c55c.chunk.css"
+  },
+  {
+    "revision": "7575087ab9adf33d49f1",
+    "url": "/estate-test/static/js/2.24691089.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/estate-test/static/js/2.24691089.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fdf859f93ebe486546c0",
+    "url": "/estate-test/static/js/main.f8592ac8.chunk.js"
+  },
+  {
+    "revision": "141a59d9f5fa2e97624c",
+    "url": "/estate-test/static/js/runtime-main.624087d4.js"
+  },
+  {
+    "revision": "63dcd398c8122c805a29cc6e1839c564",
+    "url": "/estate-test/static/media/card-cap.63dcd398.svg"
+  }
+]);
